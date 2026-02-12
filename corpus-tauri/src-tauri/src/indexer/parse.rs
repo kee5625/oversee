4cb2b6c65655cd042de2d174d272972f63dc4b83
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-12 23:58:22 +0530

    bugfixing parse

--- corpus-tauri/src-tauri/src/indexer/parse.rs
+++ corpus-tauri/src-tauri/src/indexer/parse.rs
@@ -1,6 +1,6 @@
 use pdfium_render::prelude::*;
 
-pub fn parse(self, page_num: usize, page: PdfPage<'_> ){
+pub fn parse(page_num: usize, page: PdfPage<'_> ){
     println!("=============== Page {} ===============", page_num);
     println!("{}", page.text().unwrap().all());
 }
