0def2d52564ebe7faeabe9d1d791884843032170
Karthik R <karthikeya.rachamolla@gmail.com>
2026-02-12 23:56:44 +0530

    resetting parse state

--- corpus-tauri/src-tauri/src/indexer/parse.rs
+++ corpus-tauri/src-tauri/src/indexer/parse.rs
@@ -1,18 +1,6 @@
 use pdfium_render::prelude::*;
 
-pub struct Parse{
-    pdfium: Pdfium,
-}
-
-impl Parse {
-    pub fn new() -> Result<Self, String> {
-        let pdfium = Pdfium::default();
-
-        Ok(Parse{ pdfium })
-    }
-    
-    pub fn parse(self, page_num: usize, page: PdfPage<'_> ){
-        println!("=============== Page {} ===============", page_num);
-        println!("{}", page.text().unwrap().all());
-    }
+pub fn parse(self, page_num: usize, page: PdfPage<'_> ){
+    println!("=============== Page {} ===============", page_num);
+    println!("{}", page.text().unwrap().all());
 }
